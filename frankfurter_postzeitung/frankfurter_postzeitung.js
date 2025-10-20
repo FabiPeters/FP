2fcf45bc269bf86834b55224ffee0f2c9f9a5a86
Fabian Peters <93378575+FabiPeters@users.noreply.github.com>
2025-10-20 09:55:09 +0200

    Update html css js

--- frankfurter_postzeitung/frankfurter_postzeitung.js
+++ frankfurter_postzeitung/frankfurter_postzeitung.js
@@ -1,14 +1,16 @@
+console.log("Hallo Welt.")
+
 document.querySelectorAll('input[type="checkbox"]').forEach(checkbox => {
     checkbox.addEventListener('change', event => {
         const targetClass = event.target.id.replace('toggle_', '');
         const targetElements = document.getElementsByClassName(targetClass);
         if (event.target.checked) {
             for (const el of targetElements) {
-                el.classList.add("highlight");
+                el.classList.add("date-highlight");
             }
         } else {
             for (const el of targetElements) {
-                el.classList.remove("highlight");
+                el.classList.remove("date-highlight");
             }
         }
 
